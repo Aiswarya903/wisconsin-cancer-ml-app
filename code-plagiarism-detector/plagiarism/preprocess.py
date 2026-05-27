7edaa276c0b9480b96746b9b0c571a398eafc823
Aiswarya903 <aiswaryaray05@gmail.com>
2026-05-27 16:09:57 +0530

    Delete code-plagiarism-detector/plagiarism directory

--- code-plagiarism-detector/plagiarism/preprocess.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def preprocess(code):
-    # remove comments
-    code = re.sub(r'#.*|//.*', '', code)
-    # remove extra spaces and empty lines
-    lines = [line.strip() for line in code.splitlines() if line.strip()]
-    return "\n".join(lines)
-
